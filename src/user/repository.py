300a7ad1303f3318a21cee29b42bcb31cb83995e
Inv0kerGod <122999445+Inv0kerGod@users.noreply.github.com>
2025-07-30 17:57:53 +0600

    feat: add repository for user

--- src/user/repository.py
+++ src/user/repository.py
@@ -1,1 +1,30 @@
+from typing import Sequence
 
+from sqlalchemy import select, update, delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.user.models import User as UserModel
+
+class UserRepository:
+    """Низкоуровневые операции с таблицей User"""
+
+    # 1. Список юзеров
+    @staticmethod
+    async def list_all(db: AsyncSession) -> Sequence[UserModel]:
+        result = await db.execute(select(UserModel))
+        return result.scalars().all()
+
+    # 2. Получить юзера по id
+    @staticmethod
+    async def get_by_id(db: AsyncSession, user_id: int) -> UserModel:
+        result = await db.execute(
+            select(UserModel).where(UserModel.id == user_id)
+        )
+        return result.scalars().first() # посмотреть что делает first если что поставить scalar_one_or_none()
+
+    # 3. Delete user по Id
+
+    @staticmethod
+    async def delete_user_by_id(db: AsyncSession, user_id: int) -> None:
+        await db.execute(delete(UserModel).where(UserModel.id == user_id))
+        await db.commit()
